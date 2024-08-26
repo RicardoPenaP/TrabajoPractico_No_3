1e4b10a0e5ae4313083f3a1bb0aa486c4fe36e82
RicardoPenaP <ricardopena241197@gmail.com>
2024-08-26 19:38:26 -0300

    Implemented ball collision logic by the ball view

--- Assets/Scripts/Gameplay/Entities/Ball/BallView.cs
+++ Assets/Scripts/Gameplay/Entities/Ball/BallView.cs
@@ -1,4 +1,5 @@
-﻿using Gameplay.Entities.Common.Movement;
+﻿using Gameplay.Entities.Common.Collisions;
+using Gameplay.Entities.Common.Movement;
 using System;
 using UnityEngine;
 
@@ -6,6 +7,44 @@ namespace Gameplay.Entities.Ball
 {
     public class BallView : MonoBehaviour, IMovementView
     {
-        public event Action<Vector2> OnMovementInputUpdated;
+        #region Editor Variables
+        [Header("Ball View")]
+        [Header("References")]
+        [SerializeField] private CollisionHandler _collisionHandler = null;
+        #endregion
+
+        #region Events
+        public event Action<Vector2> OnMovementInputUpdated = delegate { };
+        public event Action<Collider2D> OnBallCollides = delegate { };
+        #endregion
+
+        #region Unity Methods
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void OnDestroy()
+        {
+            Deinit();
+        }
+        #endregion
+
+        #region Callbacks
+        private void CollisionHandler_OnCollisionDetected(Collider2D collider) => OnBallCollides?.Invoke(collider);
+       
+        #endregion
+
+        #region Private Methods
+        private void Init()
+        {
+            _collisionHandler.OnCollisionDetected += CollisionHandler_OnCollisionDetected;
+        }
+
+        private void Deinit()
+        {
+            _collisionHandler.OnCollisionDetected -= CollisionHandler_OnCollisionDetected;
+        }
+        #endregion
     }
 }
