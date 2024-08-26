be5cb09f879198a0b7c926ded6579567a9d0f9cd
RicardoPenaP <ricardopena241197@gmail.com>
2024-08-26 19:29:53 -0300

    Created collision handler class

---
+++ Assets/Scripts/Gameplay/Entities/Common/Collisions/CollisionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Entities.Common.Collisions
+{
+    public class CollisionHandler : MonoBehaviour
+    {
+        #region Events
+        public event Action<Collider2D> OnCollisionDetected = delegate { };
+        #endregion
+
+        #region Unity Methods
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            OnCollisionDetected?.Invoke(collision.collider);
+        }
+        #endregion
+    }
+}
