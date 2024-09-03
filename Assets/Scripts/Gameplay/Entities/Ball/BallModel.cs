7c110ad602558499a6696931fa1b1d258f43ba9b
RicardoPenaP <ricardopena241197@gmail.com>
2024-09-02 22:30:51 -0300

    Refactorized code

--- Assets/Scripts/Gameplay/Entities/Ball/BallModel.cs
+++ Assets/Scripts/Gameplay/Entities/Ball/BallModel.cs
@@ -8,7 +8,6 @@ namespace Gameplay.Entities.Ball
         #region Editor Variables
         [Header("Ball Model")]
         [Header("References")]
-        [SerializeField] private Transform _targetTransform = null;
         [SerializeField] private Rigidbody2D _rigidbody = null;
 
         [Header("Movement Settings")]
@@ -54,7 +53,7 @@ namespace Gameplay.Entities.Ball
             _rigidbody.velocity = newMovementDirection;
         }
 
-        public void SetBallPosition(Vector3 newPosition) => _targetTransform.position = newPosition;
+        public void SetBallPosition(Vector3 newPosition) => _rigidbody.position = newPosition;  // Temp code: use rb move method and disable the view
 
         public void SleepRigidbody() => _rigidbody.Sleep();
         #endregion
